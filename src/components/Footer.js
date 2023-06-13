7bdfe60bb08b5a8b58d22a93c7ae4ef632717af4
delso-ferreira <delsofelipe@gmail.com>
2023-06-13 16:11:26 -0300

    att final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import drink from '../images/drinkIcon.svg';
 import meal from '../images/mealIcon.svg';
+import drink from '../images/drinkIcon.svg';
 import './footer.css';
 
 class Footer extends Component {
