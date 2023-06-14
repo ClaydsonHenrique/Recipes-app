5697fe664e9a13dd4c0d07023a1d0ba19dff04d2
João Victor Bernardo <jvbernardof@gmail.com>
2023-06-14 16:27:37 -0300

    Feat: Requisitos 57, 58 e 59 - Implementa a tela de Profile com os respectivos botões e recupera o email do LocalStorage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Footer from '../components/Footer';
+import React from 'react';
 
-class Profile extends Component {
-  render() {
-    return (
-      <div>
-        <div>Profile</div>
-        <Footer />
-      </div>
-    );
-  }
+function Profile() {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const userEmail = storedUser ? storedUser.email : '';
+
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <p data-testid="profile-email">
+        Email:
+        {' '}
+        {userEmail}
+      </p>
+      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn">Logout</button>
+    </div>
+  );
 }
 
-export default connect()(Profile);
+export default Profile;
